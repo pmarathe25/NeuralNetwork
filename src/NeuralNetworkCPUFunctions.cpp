980d1e7926a38e96267dcd672dcb1f0a7d4d6704
Pranav Marathe <pmarathe25@gmail.com>
2017-01-03 19:58:19 -0500

    Updates to accomodate revised matrix code.

---
+++ src/NeuralNetworkCPUFunctions.cpp
@@ -0,0 +1,14 @@
+#ifndef NEURAL_NETWORK_CPU_FUNCTIONS
+#define NEURAL_NETWORK_CPU_FUNCTIONS
+
+namespace ai {
+    template <typename T>
+    void NeuralNetwork<T>::applyActivationFunctionCPU(math::Matrix<T>& layer) const {
+        for (int i = 0; i < layer.size(); ++i) {
+            layer[i] = 1 / (1 + exp(layer[i]));
+        }
+    }
+
+}
+
+#endif
