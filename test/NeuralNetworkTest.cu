488659366821b7af8b0ba730e6f9b6787f0eb543
Pranav Marathe <pmarathe25@gmail.com>
2017-08-05 11:32:00 -0400

    Removes extraneous test code

--- test/NeuralNetworkTest.cu
+++ test/NeuralNetworkTest.cu
@@ -15,55 +15,29 @@ int main() {
     Matrix_F input({10, 7.5, 5, 2.5, 0, -2.5, -7.5, -10}, 8);
     Matrix_F expectedOutput = input.applyFunction<ai::sigmoid>();
 
-    NeuralNetwork_F<SigmoidFCL_F, ReLUFCL_F> layerTest(SigmoidFCL_F(1, 100), ReLUFCL_F(100, 1));
+    NeuralNetwork_F<SigmoidFCL_F, LeakyReLUFCL_F> layerTest(SigmoidFCL_F(1, 100), LeakyReLUFCL_F(100, 1));
     // Let's create an optimizer!
-    ai::NeuralNetworkOptimizer<Matrix_F, ai::mse_prime<Matrix_F>, SigmoidFCL_F, ReLUFCL_F> optimizer(layerTest);
+    ai::NeuralNetworkOptimizer<Matrix_F, ai::mse_prime<Matrix_F>, SigmoidFCL_F, LeakyReLUFCL_F> optimizer(layerTest);
     std::cout << "Testing Layer Manager feedForward" << std::endl;
     layerTest.feedForward(input).display();
 
-    // std::cout << "Testing Layer Manager backpropagate" << std::endl;
-    // std::cout << "Expected" << std::endl;
-    // expectedOutput.display();
-    // std::cout << "Actual" << std::endl;
-    // layerTest.backpropagate(input, expectedOutput);
-    // layerTest.feedForward(input).display();
-    //
     std::cout << "Testing Layer Manager training" << std::endl;
     std::cout << "Expected" << std::endl;
     expectedOutput.display();
     std::cout << "Actual" << std::endl;
-    optimizer.train(input, expectedOutput, 0.005);
+    optimizer.train(input, expectedOutput, 0.01);
     // layerTest.train(input, expectedOutput, 0.001);
     layerTest.feedForward(input).display();
 
     // Let's do weight saving!
-    ai::NeuralNetworkSaver<Matrix_F, SigmoidFCL_F, ReLUFCL_F> saver(layerTest);
+    ai::NeuralNetworkSaver<Matrix_F, SigmoidFCL_F, LeakyReLUFCL_F> saver(layerTest);
     saver.save("./test/networkWeights.bin");
 
     // Let's do weights loading!
-    NeuralNetwork_F<SigmoidFCL_F, ReLUFCL_F> loadingTest;
-    ai::NeuralNetworkSaver<Matrix_F, SigmoidFCL_F, ReLUFCL_F> loader(loadingTest);
+    NeuralNetwork_F<SigmoidFCL_F, LeakyReLUFCL_F> loadingTest;
+    ai::NeuralNetworkSaver<Matrix_F, SigmoidFCL_F, LeakyReLUFCL_F> loader(loadingTest);
     loader.load("./test/networkWeights.bin");
     std::cout << "Loaded Network" << '\n';
     loadingTest.feedForward(input).display();
-
-
-    // SigmoidFCL_F perfectSigmoid(1, 1);
-    // NeuralNetwork_MSE_F<SigmoidFCL_F> sigmoidNetwork(perfectSigmoid);
-    // std::cout << "Testing perfect sigmoid network before training" << std::endl;
-    // sigmoidNetwork.feedForward(input).display();
-    // std::cout << "Weights" << std::endl;
-    // perfectSigmoid.getWeights().display();
-    // std::cout << "Biases" << std::endl;
-    // perfectSigmoid.getBiases().display();
-    // std::cout << "Expected" << std::endl;
-    // expectedOutput.display();
-    // std::cout << "Actual (after training)" << std::endl;
-    // sigmoidNetwork.train(input, expectedOutput);
-    // sigmoidNetwork.feedForward(input).display();
-    // std::cout << "Weights" << std::endl;
-    // perfectSigmoid.getWeights().display();
-    // std::cout << "Biases" << std::endl;
-    // perfectSigmoid.getBiases().display();
 
 }
